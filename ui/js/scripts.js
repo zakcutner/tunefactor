862f5d56d4b8cd78a053958373993903b8a016de
Zak Cutner <me@zakcutner.uk>
2018-02-17 23:36:33 +0000

    Implementation with server

--- ui/js/scripts.js
+++ ui/js/scripts.js
@@ -15,48 +15,82 @@ $(function() {
 
   $('.next').click(function(e) {
     e.preventDefault();
-    var next = $('.owl-dot.active').next();
-    next.click();
 
-    Sortable.create($('.owl-item.active ul')[0], {
-      animation: 150
-    });
+    var next = $('.owl-dot.active').next();
 
     var ids = $('.owl-item.active li').get().map(function(el) {
       return $(el).data('id');
     });
 
-    username = $('#username').val();
+    if ($('.owl-item.active section').hasClass('initial')) {
+      Sortable.create($('.owl-item.active ul')[0], {
+        animation: 150
+      });
 
-    $.post('/login', {
-      username: $('#username').val(),
-      password: $('#password').val()
-    }, function(data) {
-      if (data.success) {
-        $.post('/api/initial_song', { username: username }, nextChallenge);
-      }
-    });
+      username = $('#username').val();
 
-    console.log(ids);
-  });
+      $.post('/login', {
+        username: $('#username').val(),
+        password: $('#password').val()
+      }, function(data) {
+        if (data.success) {
+          $.post('/api/initial_song', { username: username }, function(data) {
+            data.forEach(function(song) {
+              $('.owl-item.active').next()
+                .find('ul')
+                .append('<li data-id="' + song.id + '">' +
+                          '<img src="' + song.coverArt + '">' +
+                          '<div>' +
+                            '<p>' + song.name + '</p>' +
+                            '<p>' + song.artists + '</p>' +
+                          '</div>' +
+                          '<a href="#" class="listen">' +
+                            '<i class="fas fa-volume-up"></i>' +
+                          '</a>' +
+                        '</li>');
 
-  function nextChallenge(data) {
+              audios[song.id] = new Audio(song.previewURL);
+            });
 
-  }
+            next.click();
+          });
+        }
+      });
+    } else {
+      $.post('/api/authenticate', {
+        username: username,
+        order: ids
+      }, function(data) {
+        switch (data.mode) {
+          case 'hold':
+            data.songs.forEach(function(song) {
+              $('.owl-item.active').next()
+                .find('ul')
+                .append('<li data-id="' + song.id + '">' +
+                          '<img src="' + song.coverArt + '">' +
+                          '<div>' +
+                            '<p>' + song.name + '</p>' +
+                            '<p>' + song.artists + '</p>' +
+                          '</div>' +
+                          '<a href="#" class="listen">' +
+                            '<i class="fas fa-volume-up"></i>' +
+                          '</a>' +
+                        '</li>');
+            });
 
-  $.get('data.json', function(data) {
-    data.forEach(function(song) {
-      $('<li data-id="' + song.id + '">' +
-          '<img src="' + song.coverArt + '">' +
-          '<div>' +
-            '<p>' + song.name + '</p>' +
-            '<p>' + song.artists + '</p>' +
-          '</div>' +
-          '<a href="#" class="listen"><i class="fas fa-volume-up"></i></a>' +
-        '</li>').appendTo('ul');
-
-      audios[song.id] = new Audio(song.previewURL);
-    });
+            next.click();
+            break;
+          case 'pass':
+            alert('Passed');
+            break;
+          case 'fail':
+            alert('Failed');
+            break;
+          default:
+            console.error('Invalid mode: ' + data.mode);
+        }
+      });
+    }
   });
 
   $('ul').on('click', 'a.listen', function(e) {
